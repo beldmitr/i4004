7b0ff737b3a8e6bebd524715fad14a0c9d4dbd00
WBull <beldmitra@gmail.com>
2016-12-26 18:48:33 +0100

    Added CpuModel

---
+++ GUI/cpumodel.cpp
@@ -0,0 +1,6 @@
+#include "cpumodel.h"
+
+cpuModel::cpuModel()
+{
+
+}
---
+++ GUI/cpumodel.h
@@ -0,0 +1,12 @@
+#ifndef CPUMODEL_H
+#define CPUMODEL_H
+
+#include <QObject>
+
+class cpuModel : public QAbstractItemModel
+{
+public:
+    cpuModel();
+};
+
+#endif // CPUMODEL_H
