1a3caa6f0f383000532a0e0a885eb96fa60c49a1
WBull <beldmitra@gmail.com>
2016-12-26 18:49:44 +0100

    CpuModel editing

--- GUI/cpumodel.h
+++ GUI/cpumodel.h
@@ -2,11 +2,22 @@
 #define CPUMODEL_H
 
 #include <QObject>
+#include <QAbstractItemModel>
 
-class cpuModel : public QAbstractItemModel
+class CpuModel : public QAbstractItemModel
 {
 public:
-    cpuModel();
+    CpuModel();
+    std::vector<int> cpuData;
+
+
+    // QAbstractItemModel interface
+public:
+    QModelIndex index(int row, int column, const QModelIndex& parent) const Q_DECL_OVERRIDE;
+    QModelIndex parent(const QModelIndex& child) const Q_DECL_OVERRIDE;
+    int rowCount(const QModelIndex& parent) const Q_DECL_OVERRIDE;
+    int columnCount(const QModelIndex& parent) const Q_DECL_OVERRIDE;
+    QVariant data(const QModelIndex& index, int role) const Q_DECL_OVERRIDE;
 };
 
 #endif // CPUMODEL_H
