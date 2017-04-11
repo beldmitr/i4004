ad08d8e709ef9fb726741f5951cb14f9d544b46b
Dmytro Bilovol <dmytro.bilovol@configair.com>
2017-04-12 00:08:16 +0200

    Class Number is improved

--- Utils/number.cpp
+++ Utils/number.cpp
@@ -1,24 +1,63 @@
 #include "number.h"
 
+std::regex Utils::Number::binPrefix = std::regex("(0b[01]+)|(0B[01]+)");
+std::regex Utils::Number::binPostfix = std::regex("([01]+b)|([01]+B)");
+
+std::regex Utils::Number::dec = std::regex("[0-9]+");
+
+std::regex Utils::Number::hexPrefix = std::regex("(0x[0-f]+)|(0X[0-f]+)|(0x[0-F]+)|(0X[0-F]+)");
+std::regex Utils::Number::hexPostfix = std::regex("([0-F]+H)|([0-f]+H)|([0-F]+h)|([0-f]+h)");
+
 bool Utils::Number::isBin(const std::string& str)
 {
-    std::regex rBin("([01]+B)|([01]+b)|(0b[01]+)|(0B[01]+)");
-    return regex_match(str, rBin);
+    return regex_match(str, binPrefix) || regex_match(str, binPostfix);
 }
 
 bool Utils::Number::isDec(const std::string& str)
 {
-    std::regex rDec("[0-9]+");
-    return regex_match(str, rDec);
+    return regex_match(str, dec);
 }
 
 bool Utils::Number::isHex(const std::string& str)
 {
-    std::regex rHex("([0-F]+H)|([0-f]+H)|([0-F]+h)|([0-f]+h)|(0x[0-f]+)|(0X[0-f]+)|(0x[0-F]+)|(0X[0-F]+)");
-    return regex_match(str, rHex);
+    return regex_match(str, hexPrefix) || regex_match(str, hexPostfix);
 }
 
 bool Utils::Number::isNumber(const std::string& str)
 {
     return isBin(str) || isDec(str) || isHex(str);
+}
+
+unsigned int Utils::Number::getUInt(const std::string& str)
+{
+    if(regex_match(str, binPrefix))
+    {
+        std::string result = str.substr(2, str.size() - 2);
+        return std::stoi(result, nullptr, 2);
+    }
+    else if (regex_match(str, binPostfix))
+    {
+        std::string result = str.substr(0, str.size() - 1);
+        return std::stoi(result, nullptr, 2);
+    }
+    else if (regex_match(str, dec))
+    {
+        return std::stoi(str, nullptr, 10);
+    }
+    else if (regex_match(str, hexPrefix))
+    {
+        std::string result = str.substr(2, str.size()-2);
+        return std::stoi(result, nullptr, 16);
+    }
+    else if (regex_match(str, hexPostfix))
+    {
+        std::string result = str.substr(0, str.size() - 1);
+        return std::stoi(result, nullptr, 16);
+    }
+    else
+    {
+        std::string msg = "Number::Unknown type of a number " + str;
+        throw msg;
+    }
+
 }
--- Utils/number.h
+++ Utils/number.h
@@ -6,12 +6,22 @@
 namespace Utils {
     class Number
     {
-    public:
-        bool static isBin(const std::string& str);
-        bool static isDec(const std::string& str);
-        bool static isHex(const std::string& str);
+    private:
+        static std::regex binPrefix;    // finds 0b00000 or 0B00000 numbers
+        static std::regex binPostfix;   // finds 00000b or 00000B numbers
 
-        bool static isNumber(const std::string& str);
+        static std::regex dec;
+
+        static std::regex hexPrefix;    // finds 0x00000 or 0X00000 numbers
+        static std::regex hexPostfix;   // finds 00000h or 00000H numbers
+
+    public:
+        static bool isBin(const std::string& str);
+        static bool isDec(const std::string& str);
+        static bool isHex(const std::string& str);
+        static bool isNumber(const std::string& str);
+        static unsigned int getUInt(const std::string& str);
+
     };
 }
 
