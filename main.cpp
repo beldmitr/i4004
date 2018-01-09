565a0f4a75e8fe4f6f90c96f141c72c8ba2fdbc1
beldmitr <beldmitra@gmail.com>
2018-01-09 01:05:55 +0100

    added try catch to main

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@
 #include "Simulator/simulator.h"
 #include "GUI/mainwindow.h"
 
+#include "Exceptions/compilerexception.h"
+#include "Exceptions/logexceptions.h"
+
 int main (int argc, char* argv[])
 {
     std::shared_ptr<Compiler> compiler;
@@ -16,16 +19,28 @@ int main (int argc, char* argv[])
     std::shared_ptr<QApplication> app;
     std::shared_ptr<MainWindow> parent;
 
-    // Create Compiler
-    compiler = std::shared_ptr<Compiler>(new Compiler);
+    try
+    {
+        // Create Compiler
+        compiler = std::shared_ptr<Compiler>(new Compiler);
 
-    // Create Simulator
-    simulator = std::shared_ptr<Simulator>(new Simulator(*compiler.get()));
+        // Create Simulator
+        simulator = std::shared_ptr<Simulator>(new Simulator(*compiler.get()));
 
-    // Create GUI
-    app = std::shared_ptr<QApplication>(new QApplication(argc, argv));
-    parent = std::shared_ptr<MainWindow>(new MainWindow(*compiler.get(), *simulator.get()));
-    parent->show();
+        // Create GUI
+        app = std::shared_ptr<QApplication>(new QApplication(argc, argv));
+        parent = std::shared_ptr<MainWindow>(new MainWindow(*compiler.get(), *simulator.get()));
+        parent->show();
+    }
+    catch(const CompilerException& ex)
+    {
+        std::cerr << "Compiler exception : " << ex.what() << " from " << ex.who() << std::endl;
+    }
+    catch(const LogExceptions& ex)
+    {
+        std::cerr << "Log exception : " << ex.what() << " from " << ex.who() << std::endl;
+    }
+
 
     return app->exec();
 }
